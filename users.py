7925064e94f2dc47629ae2312802483facc408fe
xrpew <spergio8@gmail.com>
2023-03-06 08:31:49 -0300

    agregando link al curso

--- users.py
+++ users.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
-from pydantic import BasModel
+#from pydantic import BasModel
 
 app = FastAPI()
 
-class User(BasModel)
+#class User(BasModel)
 
 @app.get('/users')
 def users():
-    return {'msg':'hola users'}
+    return [{'name':'Sergio', 'surname':'xerpew', 'url':'xerpew.com'},
+            {'name':'Alejandro', 'surname':'dev', 'url':'alejandro.com'},
+            {'name':'Sergio', 'surname':'xerpew', 'url':'xerpew.com'}]
 
